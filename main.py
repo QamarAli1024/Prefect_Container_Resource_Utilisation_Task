fce7f3be548a4a16de9910c3ee7ef21bb5f13ed1
Qamar Ali <Qamar.Ali@kencogroup.com>
2024-05-09 22:49:00 +0530

    Refactoring to have a single task, instead of many.

--- main.py
+++ main.py
@@ -55,83 +55,55 @@ def get_task_definition_families():
     return list(task_definition_families)
 
 @task
-def fetch_ecs_metrics(cloudwatch, cluster_name, task_definition_family):
+def fetch_ecs_metrics_all(cloudwatch, task_definition_families, rows_list):
     # Define the time period for the metrics
     end_time = datetime.utcnow()
     start_time = end_time - timedelta(days=7)  # Last 7 days
     print(start_time, end_time)
 
-    # Helper to fetch metrics
-    def get_metric(metric_name, stat):
-        response = cloudwatch.get_metric_data(
-            MetricDataQueries=[
-                {
-                    'Id': 'm1',
-                    'MetricStat': {
-                        'Metric': {
-                            'Namespace': 'ECS/ContainerInsights',  # 'AWS/ECS',
-                            'MetricName': metric_name,
-                            'Dimensions': [
-                                {'Name': 'ClusterName', 'Value': cluster_name},
-                                {'Name': 'TaskDefinitionFamily', 'Value': task_definition_family}
-                            ]
-                        },
-                        'Period': 7 * 86400,  # 7 days
-                        'Stat': stat,
-                    },
-                    'ReturnData': True,
-                },
-            ],
-            StartTime=start_time,
-            EndTime=end_time
-        )
-        # print(response)
-        return response['MetricDataResults'][0]['Values']
-
-    # Fetch CPU and memory metrics
-    cpu_utilization = get_metric('CpuUtilized', 'Average')
-    cpu_max = get_metric('CpuUtilized', 'Maximum')
-    cpu_reserved = get_metric('CpuReserved', 'Average')
-    memory_utilization = get_metric('MemoryUtilized', 'Average')
-    memory_max = get_metric('MemoryUtilized', 'Maximum')
-    memory_reserved = get_metric('MemoryReserved', 'Average')
-
-    return cpu_utilization, cpu_max, cpu_reserved, memory_utilization, memory_max, memory_reserved
-
-
-@flow(name='prefect-container-utilisation-task-flow', retries=3, retry_delay_seconds=30, log_prints=True)
-def main(config=None):
-    # config = config if config else {}
-    # logger = get_run_logger()
-    # logger.info('Entering...')
-    # logger.info(config.get('alpha', 'No Parameter Passed!'))
-    # res = basic()
-
-    # Create a CloudWatch client
-    boto3_login = {
-        "verify": False,
-        "service_name": 'cloudwatch',
-        "region_name": 'us-east-1',
-        "aws_access_key_id": get_secret("AWS_ACCESS_KEY_ID"),
-        "aws_secret_access_key": get_secret("AWS_SECRET_ACCESS_KEY")
-    }
-    cloudwatch = boto3.client(**boto3_login)
-
-    task_definition_families = get_task_definition_families()
-    print("Number of task_definition_families:", len(task_definition_families))
-
-    rows_list = []
     for task_definition_family in task_definition_families:
         for cluster_name in ["ECSClusterProd", "ECSClusterDev"]:
-            cpu_util, cpu_max, cpu_reserved, mem_util, mem_max, mem_reserved = fetch_ecs_metrics(cloudwatch, cluster_name,
-                                                                                                 task_definition_family)
+            # Helper to fetch metrics
+            def get_metric(metric_name, stat):
+                response = cloudwatch.get_metric_data(
+                    MetricDataQueries=[
+                        {
+                            'Id': 'm1',
+                            'MetricStat': {
+                                'Metric': {
+                                    'Namespace': 'ECS/ContainerInsights',  # 'AWS/ECS',
+                                    'MetricName': metric_name,
+                                    'Dimensions': [
+                                        {'Name': 'ClusterName', 'Value': cluster_name},
+                                        {'Name': 'TaskDefinitionFamily', 'Value': task_definition_family}
+                                    ]
+                                },
+                                'Period': 7 * 86400,  # 7 days
+                                'Stat': stat,
+                            },
+                            'ReturnData': True,
+                        },
+                    ],
+                    StartTime=start_time,
+                    EndTime=end_time
+                )
+                # print(response)
+                return response['MetricDataResults'][0]['Values']
+
+            # Fetch CPU and memory metrics
+            cpu_utilization = get_metric('CpuUtilized', 'Average')
+            cpu_max = get_metric('CpuUtilized', 'Maximum')
+            cpu_reserved = get_metric('CpuReserved', 'Average')
+            memory_utilization = get_metric('MemoryUtilized', 'Average')
+            memory_max = get_metric('MemoryUtilized', 'Maximum')
+            memory_reserved = get_metric('MemoryReserved', 'Average')
+
+            cpu_util, cpu_max, cpu_reserved, mem_util, mem_max, mem_reserved = cpu_utilization, cpu_max, cpu_reserved, memory_utilization, memory_max, memory_reserved
+
             print(task_definition_family)
-            print("CPU Utilization:", cpu_util)
-            print("CPU Max:", cpu_max)
-            print("CPU Reserved:", cpu_reserved)
-            print("Memory Utilization:", mem_util)
-            print("Memory Max:", mem_max)
-            print("Memory Reserved:", mem_reserved)
+            print(f"CPU Utilization: {cpu_util}; CPU Max: {cpu_max}; "
+                  f"CPU Reserved: {cpu_reserved}; Memory Utilization: {mem_util}"
+                  f"Memory Max: {mem_max}; Memory Reserved: {mem_reserved}")
 
             if (cpu_util and cpu_max and cpu_reserved and
                     mem_util and mem_max and mem_reserved):
@@ -155,6 +127,32 @@ def main(config=None):
                                       'CPU Savings': round(cpu_savings), 'Memory Savings': round(mem_savings),
                                       'CPU + Memory Savings': round(cpu_savings + mem_savings)})
 
+    return rows_list
+
+@flow(name='prefect-container-utilisation-task-flow', retries=3, retry_delay_seconds=30, log_prints=True)
+def main(config=None):
+    # config = config if config else {}
+    # logger = get_run_logger()
+    # logger.info('Entering...')
+    # logger.info(config.get('alpha', 'No Parameter Passed!'))
+    # res = basic()
+
+    # Create a CloudWatch client
+    boto3_login = {
+        "verify": False,
+        "service_name": 'cloudwatch',
+        "region_name": 'us-east-1',
+        "aws_access_key_id": get_secret("AWS_ACCESS_KEY_ID"),
+        "aws_secret_access_key": get_secret("AWS_SECRET_ACCESS_KEY")
+    }
+    cloudwatch = boto3.client(**boto3_login)
+
+    task_definition_families = get_task_definition_families()
+    print("Number of task_definition_families:", len(task_definition_families))
+
+    rows_list = []
+    rows_list = fetch_ecs_metrics_all(cloudwatch, task_definition_families, rows_list)
+
     df = pd.DataFrame(rows_list)
     df = df.sort_values('CPU + Memory Savings', ascending=False)
     print(df)
@@ -163,12 +161,9 @@ def main(config=None):
                          "<h3>Please find data attached for tasks falling above "
                          "or below 50%-80% threshold for CPU and Memory resource-utilization.</h3>", False)
     email.embed_df_as_html_table(df, color='blue_light', font_size=14, text_align='center')
-
     recipients = get_secret('DEV_TEAM')
-
-    email.send(list(set(recipients)))  # unique recipients
-
-    print('Email Sent.')
+    # email.send(list(set(recipients)))  # unique recipients
+    # print('Email Sent.')
 
     # return res
     return
